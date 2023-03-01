9bd4d05caf1d383daf153000b4c18884b431268e
DavidMacdillen <104839637+DavidMacdillen@users.noreply.github.com>
2023-03-01 16:00:23 -0300

    Add files via upload

---
+++ ToDoApi1/Moldes/usuarioModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoApi1.Moldes
+{
+    public class usuarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
